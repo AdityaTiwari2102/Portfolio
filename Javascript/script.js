5efe8738e392a9d7aeae9f47c22f65309a4a20f2
Aditya Tiwari <taditya.tiwari007@gmail.com>
2017-06-19 00:20:24 +0530

    Add files via upload

---
+++ Javascript/script.js
@@ -0,0 +1,57 @@
+$(document).ready(function() {
+
+  $(window).resize(function(){location.reload();});
+
+  if ( $( window ).width() > 708 ){
+    $('a').click(function() {
+      $('a').removeClass('selected');
+      $(this).addClass('selected');
+      current = $(this);
+      $('.wrapper').scrollTo($(this).attr('href'), 800);
+      return false;
+    });
+
+    $('.quote').typeIt({
+     strings: ["Hi, I am Aditya.", "I believe a great design can make a good idea even better."],
+     speed: 100,
+     autoStart: true
+  });
+
+    $(".move-ahead").delay(8500).show("fade", 2000);
+  }
+
+  $('.nav-menu-bars').click(function(){
+    $( ".nav-menu" ).toggle("fade", "slow");
+    //$(this).find('i').toggleClass('fa-bars fa-arrow-right')
+  });
+
+  
+  if ( $( window ).width() < 708 ){
+    $(".nav-menu-bars").click(function() { 
+      return (this.tog = !this.tog) ? menu() : menuClose();
+    });
+
+    $('.nav-menu ul li a').click(function(){
+     $('.nav-menu').hide();
+     $('body').animate({ left: "0px"});
+   });
+
+   $('.quote').typeIt({
+     strings: ["I believe a great design can make a good idea even better."],
+     speed: 100,
+     autoStart: true
+  });
+
+  $(".move-ahead").delay(10000).show("fade", 2000);
+}
+  
+});
+
+function menu() {
+    $('body').animate({ left: "130px"}, 200);
+}
+
+function menuClose() {
+  $( ".nav-menu" ).hide();
+  $('body').animate({ left: "0px"}, 200);
+}
